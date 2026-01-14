0decf9a91017a1f2b4b4763b86d38f85a119bbea
chen <wszgrcy@gmail.com>
2026-01-14 10:35:09 +0800

    status

--- src/app/page/component/status.ts
+++ src/app/page/component/status.ts
@@ -2,6 +2,7 @@ import * as v from 'valibot';
 import { actions, NFCSchema, setComponent } from '@piying/view-angular-core';
 import { AccountService } from '../../service/account.service';
 import { LocalSaveService } from '../../service/local-save.service';
+import { ApiService } from '../../service/api.service';
 export const StatusDefine = v.pipe(
   v.object({
     prefix: v.pipe(
@@ -34,6 +35,33 @@ export const StatusDefine = v.pipe(
       actions.props.patch({ labelPosition: 'left' }),
       actions.class.top('w-full *:last:w-full gap-4'),
       actions.class.component('w-full'),
+    ),
+    status: v.pipe(
+      v.object({
+        databaseConnectivity: v.pipe(
+          NFCSchema,
+          setComponent('common-data'),
+          actions.wrappers.set(['label-wrapper', 'div']),
+          actions.props.patch({ labelPosition: 'left' }),
+          actions.class.top('gap-4'),
+
+          v.title('databaseConnectivity'),
+          actions.inputs.patch({ content: '' }),
+        ),
+      }),
+      actions.hooks.merge({
+        allFieldsResolved: (field) => {
+          let api: ApiService = field.context['api'];
+          api.Health().subscribe((result) => {
+            field.get(['databaseConnectivity'])!.inputs.update((value) => {
+              return {
+                ...value,
+                content: result.databaseConnectivity,
+              };
+            });
+          });
+        },
+      }),
     ),
     __button: v.pipe(
       NFCSchema,
